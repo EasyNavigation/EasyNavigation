c0d30b3a7891b1d75724910c08a7f26500bf9bd5
fmrico <fmrico@users.noreply.github.com>
2025-09-23 09:42:40 +0000

    deploy: cb890cea3b8d44a2755aaf2cd1d0ee6844247d9b

--- search/functions_0.js
+++ search/functions_0.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['as_5fpoints_0',['as_points',['../classeasynav_1_1PointPerceptionsOpsView.html#a805d236592ccedb4d00c893bbf797bcd',1,'easynav::PointPerceptionsOpsView::as_points() const'],['../classeasynav_1_1PointPerceptionsOpsView.html#a94c012d5f2d51299d766adc087103ef7',1,'easynav::PointPerceptionsOpsView::as_points(int idx) const']]]
+  ['add_0',['add',['../classeasynav_1_1PointPerceptionsOpsView.html#afca194fc92002fa9a7c5102e153f750b',1,'easynav::PointPerceptionsOpsView']]],
+  ['as_5fpoints_1',['as_points',['../classeasynav_1_1PointPerceptionsOpsView.html#a805d236592ccedb4d00c893bbf797bcd',1,'easynav::PointPerceptionsOpsView::as_points() const'],['../classeasynav_1_1PointPerceptionsOpsView.html#a94c012d5f2d51299d766adc087103ef7',1,'easynav::PointPerceptionsOpsView::as_points(int idx) const']]]
 ];
