79aa6212b2aea0c510302ca667c44628569a1e25
fmrico <fmrico@users.noreply.github.com>
2026-01-07 11:15:18 +0000

    deploy: 439a8400a57caad388cd78125daa62eff6246be5

--- search/all_5.js
+++ search/all_5.js
@@ -4,5 +4,5 @@ var searchData=
   ['frame_1',['frame',['../structeasynav_1_1PointPerceptionBufferType.html#abe60a2ebe324a7ec86e5bd0024febf2e',1,'easynav::PointPerceptionBufferType']]],
   ['frame_5fid_2',['frame_id',['../classeasynav_1_1PerceptionBase.html#aee63db9a1c81df1506964242aacdac88',1,'easynav::PerceptionBase']]],
   ['full_3',['full',['../classeasynav_1_1CircularBuffer.html#adcca6ae0a0952049b1e0c8dddaa42044',1,'easynav::CircularBuffer']]],
-  ['fuse_4',['fuse',['../classeasynav_1_1PointPerceptionsOpsView.html#a94357f50a6a046bdda48145d9bab8605',1,'easynav::PointPerceptionsOpsView']]]
+  ['fuse_4',['fuse',['../classeasynav_1_1PointPerceptionsOpsView.html#a600de21cd2f773753f5b6130b71caedc',1,'easynav::PointPerceptionsOpsView::fuse(const std::string &amp;target_frame, bool exact_time=true)'],['../classeasynav_1_1PointPerceptionsOpsView.html#ac121a6cbc8ee6fe79e3368be78ba9d80',1,'easynav::PointPerceptionsOpsView::fuse(const std::string &amp;target_frame, rclcpp::Time &amp;stamp, bool exact_time=true)']]]
 ];
--- search/functions_5.js
+++ search/functions_5.js
@@ -2,5 +2,5 @@ var searchData=
 [
   ['filter_0',['filter',['../classeasynav_1_1PointPerceptionsOpsView.html#aea26f51c08e637df3671eba56902a54a',1,'easynav::PointPerceptionsOpsView']]],
   ['full_1',['full',['../classeasynav_1_1CircularBuffer.html#adcca6ae0a0952049b1e0c8dddaa42044',1,'easynav::CircularBuffer']]],
-  ['fuse_2',['fuse',['../classeasynav_1_1PointPerceptionsOpsView.html#a94357f50a6a046bdda48145d9bab8605',1,'easynav::PointPerceptionsOpsView']]]
+  ['fuse_2',['fuse',['../classeasynav_1_1PointPerceptionsOpsView.html#a600de21cd2f773753f5b6130b71caedc',1,'easynav::PointPerceptionsOpsView::fuse(const std::string &amp;target_frame, bool exact_time=true)'],['../classeasynav_1_1PointPerceptionsOpsView.html#ac121a6cbc8ee6fe79e3368be78ba9d80',1,'easynav::PointPerceptionsOpsView::fuse(const std::string &amp;target_frame, rclcpp::Time &amp;stamp, bool exact_time=true)']]]
 ];
