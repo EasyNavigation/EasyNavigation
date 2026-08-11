2ae858a1436da4c40ef39aee0f312d29be950440
fmrico <fmrico@users.noreply.github.com>
2026-08-11 19:38:04 +0000

    deploy: 04c3ecf33c5a235bda6b1fa8f9c12db6c9fdc4ff

--- search/variables_d.js
+++ search/variables_d.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['sensor_5fname_5f_0',['sensor_name_',['../classeasynav_1_1PerceptionHandler.html#a5d6c5443e56e32a307cc9a9c653d8f46',1,'easynav::PerceptionHandler::sensor_name_'],['../classPerceptionHandler.html#a5d6c5443e56e32a307cc9a9c653d8f46',1,'PerceptionHandler::sensor_name_']]],
-  ['singleton_3c_20c_20_3e_3a_3ainit_5fflag_5f_1',['init_flag_',['../namespaceeasynav.html#a76e3d54d282b01dd9dd17b2dc4564c24',1,'easynav']]],
-  ['singleton_3c_20c_20_3e_3a_3ainstance_5f_2',['instance_',['../namespaceeasynav.html#af63183b88c39f568dffa0d77e441e468',1,'easynav']]],
+  ['singleton_3c_20c_20_3e_3a_3ainstance_5f_1',['instance_',['../namespaceeasynav.html#af63183b88c39f568dffa0d77e441e468',1,'easynav']]],
+  ['singleton_3c_20c_20_3e_3a_3amutex_5f_2',['mutex_',['../namespaceeasynav.html#aea6f80498fbc28235e7a690b577c46fd',1,'easynav']]],
   ['stamp_3',['stamp',['../classeasynav_1_1PerceptionBase.html#a3ace6024cedb403facddccc6842dce24',1,'easynav::PerceptionBase::stamp'],['../structeasynav_1_1PointPerceptionBufferType.html#a3ace6024cedb403facddccc6842dce24',1,'easynav::PointPerceptionBufferType::stamp']]]
 ];
